b0dd1f2abe53e451a1a7c313d8c7256d52510d10
BilinSun02 <BilinSun02@outlook.com>
2026-08-29 02:02:25 +0000

    Add config interface

---
+++ src/config.h
@@ -0,0 +1,18 @@
+#ifndef MACOS_TRACKPOINT_SCROLL_CONFIG_H
+#define MACOS_TRACKPOINT_SCROLL_CONFIG_H
+
+#include <stdbool.h>
+
+struct macos_trackpoint_config {
+    bool natural_scroll;
+    double scroll_scale;
+    bool suppress_middle_click;
+};
+
+void macos_trackpoint_config_defaults(struct macos_trackpoint_config *cfg);
+int macos_trackpoint_config_load(struct macos_trackpoint_config *cfg,
+                                 const char *path,
+                                 bool verbose);
+const char *macos_trackpoint_default_config_path(char *buffer, unsigned long size);
+
+#endif
