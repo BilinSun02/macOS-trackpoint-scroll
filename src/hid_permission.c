08fd8db7bda6f55f31f4974c21f2061c937440f9
BilinSun02 <BilinSun02@outlook.com>
2026-08-29 05:46:58 +0000

    Request IOHID Input Monitoring from daemon

---
+++ src/hid_permission.c
@@ -0,0 +1,25 @@
+#include <IOKit/hid/IOHIDLib.h>
+
+#include <stdbool.h>
+#include <stdio.h>
+
+__attribute__((constructor))
+static void
+request_hid_input_monitoring(void)
+{
+    IOHIDAccessType access = IOHIDCheckAccess(kIOHIDRequestTypeListenEvent);
+
+    if (access == kIOHIDAccessTypeGranted) {
+        fprintf(stderr, "trackpoint: IOHID Input Monitoring access granted\n");
+        return;
+    }
+
+    fprintf(stderr,
+            "trackpoint: IOHID Input Monitoring access=%d; requesting access\n",
+            (int)access);
+
+    if (!IOHIDRequestAccess(kIOHIDRequestTypeListenEvent))
+        fprintf(stderr,
+                "trackpoint: IOHID Input Monitoring request not granted; "
+                "enable macOS-trackpoint-scroll in Privacy & Security > Input Monitoring\n");
+}
