5c7d8f493e4d8661bd6dde9470db12bac6cbf38c
BilinSun02 <BilinSun02@outlook.com>
2026-08-29 02:07:35 +0000

    Release cursor pin immediately on logical button up

--- src/cursor_pin.c
+++ src/cursor_pin.c
@@ -8,6 +8,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "cursor_pin.h"
+
 #define CURSOR_PIN_VENDOR_ID  0x5859
 #define CURSOR_PIN_PRODUCT_ID 0x0001
 #define CURSOR_PIN_MIDDLE_BUTTON_USAGE 3
@@ -17,6 +19,13 @@ static CFRunLoopTimerRef g_pin_timer;
 static bool g_pin_middle_down;
 static bool g_pin_have_anchor;
 static CGPoint g_pin_anchor;
+
+void
+cursor_pin_release_now(void)
+{
+    g_pin_middle_down = false;
+    g_pin_have_anchor = false;
+}
 
 static CFMutableDictionaryRef
 cursor_pin_matching_dictionary(void)
@@ -104,7 +113,7 @@ cursor_pin_input_value(void *context, IOReturn result, void *sender,
     if (down)
         g_pin_have_anchor = cursor_pin_get_position(&g_pin_anchor);
     else
-        g_pin_have_anchor = false;
+        cursor_pin_release_now();
 }
 
 static void
