88e7a180f1de037b1896a5b90669fbd9479ccb1c
BilinSun02 <BilinSun02@outlook.com>
2026-09-09 20:03:26 -0500

    Revert unhelpful LaunchAgent bundle association

--- scripts/install-user.sh
+++ scripts/install-user.sh
@@ -115,8 +115,6 @@ cat >"$PLIST" <<EOF
 <dict>
     <key>Label</key>
     <string>$LABEL</string>
-    <key>AssociatedBundleIdentifiers</key>
-    <string>$LABEL</string>
     <key>ProgramArguments</key>
     <array>
         <string>$INSTALL_BIN</string>
