6a8a6a0d1f76146129e28466dd057ad080656743
BilinSun02 <BilinSun02@outlook.com>
2026-09-09 21:06:18 -0500

    Expose complete virtual-HID pointing report API

--- src/edge_pressure_client.h
+++ src/edge_pressure_client.h
@@ -7,5 +7,9 @@
 void tpsc_edge_pressure_client_set_enabled(bool enabled);
 bool tpsc_edge_pressure_post(int64_t dx, int64_t dy);
 bool tpsc_edge_pressure_post_state(int64_t dx, int64_t dy, uint32_t buttons);
+bool tpsc_edge_pressure_post_report(int64_t dx, int64_t dy,
+                                    int64_t vertical_wheel,
+                                    int64_t horizontal_wheel,
+                                    uint32_t buttons);
 
 #endif
