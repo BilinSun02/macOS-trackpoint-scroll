8fe61267b8f51f7ebabd042c21bae1e49993e67a
BilinSun02 <BilinSun02@outlook.com>
2026-09-09 20:57:33 -0500

    Expose virtual-HID pointing state helper

--- src/edge_pressure_client.h
+++ src/edge_pressure_client.h
@@ -6,5 +6,6 @@
 
 void tpsc_edge_pressure_client_set_enabled(bool enabled);
 bool tpsc_edge_pressure_post(int64_t dx, int64_t dy);
+bool tpsc_edge_pressure_post_state(int64_t dx, int64_t dy, uint32_t buttons);
 
 #endif
