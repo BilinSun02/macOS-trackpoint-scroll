9a4383b7cc857073cea8fbd9d31fd7dd3bd9d408
BilinSun02 <BilinSun02@outlook.com>
2026-09-12 12:09:51 -0500

    Keep user daemon alive after unexpected exit

--- scripts/install-user.sh
+++ scripts/install-user.sh
@@ -125,6 +125,8 @@ cat >"$PLIST" <<EOF
     </array>
     <key>RunAtLoad</key>
     <true/>
+    <key>KeepAlive</key>
+    <true/>
     <key>ProcessType</key>
     <string>Interactive</string>
     <key>StandardOutPath</key>
